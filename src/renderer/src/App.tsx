ff4fedbe8070c5d6bf7e4639ed84f5def545d0a4
lijilei <lijileigood@gmail.com>
2024-10-31 16:36:46 +0800

    集成markdown-it, 初步测试成功

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,18 +1,57 @@
+import markdownCss from "./assets/github-markdown.css";
+import MarkdownIt from 'markdown-it';
 
 function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  const md = new MarkdownIt({
+    html: true,
+    linkify: true,
+    typographer: true
+  });
+
+  function makeEditable(element) {
+    let textarea = document.createElement('textarea');
+    textarea.value = element.textContent;
+    element.parentNode.replaceChild(textarea, element);
+    textarea.focus();
+
+    // 当用户完成编辑时，将`textarea`转换回文本
+    textarea.addEventListener('blur', function() {
+      let newP = document.createElement('p');
+      newP.textContent = textarea.value;
+      textarea.parentNode.replaceChild(newP, textarea);
+    });
+  }
+
+
+  let abc = `
+  # abc
+  # b
+  # b
+  # b
+  # b
+  # b
+  # c
+# b
+  \`\`\`js
+  let
+  \`\`\`
+  # b
+  # b
+  # b
+  # b
+  # b
+  # b
+  # b
+  # b
+  <img align='left'  src="https://octodex.github.com/images/minion.png" alt="image-20241030155606372" style="zoom: 10%;" />
+  `
 
   return (
     <>
-      <div className="actions">
-        <div className="action">
-          <button onClick={ipcHandle}>
-            Send IPC
-          </button>
-        </div>
-      </div>
+      <div className='markdownBody'
+           dangerouslySetInnerHTML={{ __html: md.render(abc)}} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
